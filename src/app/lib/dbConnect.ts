2cc2a311d23a222ba68ae3a337a89333dc30ee77
Gautam Bisht <bishtgautam012@gmail.com>
2024-04-20 17:55:21 +0530

    db connection

--- src/app/lib/dbConnect.ts
+++ src/app/lib/dbConnect.ts
@@ -13,7 +13,6 @@ async function dbConnect(): Promise<void> {
   }
   try {
     const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
-
     connection.isConnected = db.connections[0].readyState;
     console.log("DB connected successfully");
   } catch (error) {
